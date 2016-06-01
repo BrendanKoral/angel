53335ea3ff7c9133192c029173204e57c82165c6
Brendan Koral <brendanhkoral@gmail.com>
2016-05-31 22:29:41 -0500

    Fix the stupid git conflicts

--- js/app.js
+++ js/app.js
@@ -5,12 +5,11 @@ var myApp = angular.module('myApp', [
 
 myApp.config(['$routeProvider', function($routeProvider){
   $routeProvider.
-<<<<<<< HEAD
   when ('/home', {
     templateUrl: 'partials/home.html',
     controller: 'ListController'
   }).
-  
+
   when ('/contact', {
     templateUrl: 'partials/contact.html',
     controller: 'ListController'
@@ -26,17 +25,5 @@ myApp.config(['$routeProvider', function($routeProvider){
   }).
   otherwise({
     redirectTo: '/home'
-=======
-  when ('/list', {
-    templateUrl: 'lib/partials/list.html',
-    controller: 'ListController'
-  }).
-  when('/details/:itemId', {
-    templateUrl: 'lib/partials/details.html',
-    controller: 'DetailsController'
-  }).
-  otherwise({
-    redirectTo: '/list'
->>>>>>> 560bdc248f9cc8f316844e23c0be38bb58025c38
   });
 }]);
