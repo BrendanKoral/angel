3047a69d05d248e67599b9979fbc445bdab6f303
Brendan Koral <brendanhkoral@gmail.com>
2016-05-31 22:18:25 -0500

    Set up basic AngularJS

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'artistControllers'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+  when ('/home', {
+    templateUrl: 'partials/home.html',
+    controller: 'ListController'
+  }).
+  
+  when ('/contact', {
+    templateUrl: 'partials/contact.html',
+    controller: 'ListController'
+  }).
+
+  when('/sponsors', {
+    templateUrl: 'partials/sponsors.html',
+    controller: 'DetailsController'
+  }).
+  when('/gallery', {
+    templateUrl: 'partials/gallery.html',
+    controller: 'DetailsController'
+  }).
+  otherwise({
+    redirectTo: '/home'
+  });
+}]);
